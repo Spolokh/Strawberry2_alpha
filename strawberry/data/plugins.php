080f8e043fc553ede40d5624476c5a4f04b17297
Spolokh <54540979+Spolokh@users.noreply.github.com>
2021-01-15 15:46:12 +0300

    Add files via upload

---
+++ strawberry/data/plugins.php
@@ -0,0 +1,14 @@
+<?php
+$array = array (
+  'registration.php' => true,
+  'etc.php' => true,
+  'ddb.php' => true,
+  'eval.php' => true,
+  'post-type.php' => true,
+  'keywords.php' => true,
+  'old-style-tags.php' => true,
+  'kavychker.php' => true,
+  'quixplorer.php' => true,
+);
+
+?>
